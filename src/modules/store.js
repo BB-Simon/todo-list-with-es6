68f321c09491adc5656fb4f588fea4ec67b67518
BB-Simon <simon.chowdery@gmail.com>
2023-02-21 21:08:48 +0200

    Add todo list structure

---
+++ src/modules/store.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    index: 1,
+    description: 'Going to university',
+    compeleted: false,
+  },
+  {
+    index: 2,
+    description: 'Doing math homework',
+    compeleted: false,
+  },
+  {
+    index: 3,
+    description: 'Visiting grandmom',
+    compeleted: false,
+  },
+  {
+    index: 4,
+    description: 'Hanging out with frinds',
+    compeleted: false,
+  },
+];
