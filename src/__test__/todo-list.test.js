b007a185332425668b190a49fabbdc9952962d6a
BB-Simon <simon.chowdery@gmail.com>
2023-03-04 01:21:37 +0200

    Add tests os edit, complete, remove all complete and reset todos

--- src/__test__/todo-list.test.js
+++ src/__test__/todo-list.test.js
@@ -4,7 +4,7 @@ import store from '../__mocks__/store.js';
 
 jest.mock('../modules/todo-list.js');
 
-const todos = [];
+let todos = [];
 
 const mockAdd = jest.fn((newTask) => {
   const index = todos.length + 1;
@@ -28,10 +28,10 @@ const mockComplete = jest.fn((index) => {
 
 const mockRemoveCompleted = jest.fn(() => {
   todos = todos.filter((todo) => !todo.completed);
+  store.setItem('todos', todos);
 });
 
 const mockSortedTodos = jest.fn(() => todos.sort((a, b) => a.index - b.index));
-
 
 const mockResetTodos = jest.fn(() => {
   todos.forEach((todo, index) => {
@@ -45,35 +45,71 @@ Todolist.mockImplementation(() => ({
   edit: mockEdit,
   complete: mockComplete,
   sortedTodos: mockSortedTodos,
-  resestTodos: mockResetTodos,
-  removeCompleted: mockRemoveCompleted
+  resetTodos: mockResetTodos,
+  removeCompleted: mockRemoveCompleted,
 }));
+
+const todoList = new Todolist(todos);
 
 describe('Test todo list', () => {
   test('Add new item to todolist and store', () => {
-    const todoList = new Todolist(todos);
     todoList.add('Add new task');
     expect(todos).toHaveLength(1);
     expect(store.getItem('todos')).toHaveLength(1);
   });
 
   test('Add another item to todolist and store', () => {
-    const todoList = new Todolist(todos);
     todoList.add('Add item 2');
     expect(todos).toHaveLength(2);
     expect(store.getItem('todos')).toHaveLength(2);
   });
 
+  test('Add another item to todolist and store', () => {
+    todoList.add('Add item 3');
+    expect(todos).toHaveLength(3);
+    expect(store.getItem('todos')).toHaveLength(3);
+  });
+
   test('Remove an item from todolist and store', () => {
-    const todoList = new Todolist(todos);
+    todoList.remove(0);
+    expect(todos).toHaveLength(2);
+    expect(store.getItem('todos')).toHaveLength(2);
+  });
+
+  test('Reset todo list indexes', () => {
+    todoList.resetTodos();
+    expect(todos[0].index).toEqual(1);
+    expect(todos[1].index).toEqual(2);
+  });
+
+  test('Remove another item from todolist and store', () => {
     todoList.remove(0);
     expect(todos).toHaveLength(1);
     expect(store.getItem('todos')).toHaveLength(1);
   });
 
-  test('Remove another item from todolist and store', () => {
-    const todoList = new Todolist(todos);
-    todoList.remove(0);
+  test('Reset todo list indexes', () => {
+    todoList.resetTodos();
+    expect(todos[0].index).toEqual(1);
+  });
+
+  test('Edit first item of todo list and store', () => {
+    const updatedDescription = 'Updated description';
+    todoList.edit(0, updatedDescription);
+    expect(todos[0].description).toEqual('Updated description');
+    expect(store.getItem('todos')[0].description).toEqual('Updated description');
+  });
+
+  test('Mark first item of todo list and store as completed', () => {
+    todoList.complete(0);
+    expect(todos[0].completed).toBeTruthy();
+    expect(todos[0].completed).toEqual(true);
+    expect(store.getItem('todos')[0].completed).toBeTruthy();
+    expect(store.getItem('todos')[0].completed).toEqual(true);
+  });
+
+  test('Clear all completed todos from todo list and store', () => {
+    todoList.removeCompleted();
     expect(todos).toHaveLength(0);
     expect(store.getItem('todos')).toHaveLength(0);
   });
